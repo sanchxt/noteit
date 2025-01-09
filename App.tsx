e88c097bf9516b0feeff191b8c9b3604100f1fef
sanchxt <tpbeast007@gmail.com>
2025-01-09 16:05:47 +0530

    add provider & toast, dispatch notes and theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import { Provider } from 'react-redux';
+import React, { useEffect } from 'react';
+import Toast from 'react-native-toast-message';
+
+import { store } from './src/store/store';
+import { loadNotes } from './src/store/slices/notesSlice';
+import { loadTheme } from './src/store/slices/themeSlice';
+import { AppNavigator } from './src/navigation/AppNavigator';
+import './globals.css';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    store.dispatch(loadNotes());
+    store.dispatch(loadTheme());
+  }, []);
+
   return (
-    <View>
-      <Text>yoyo</Text>
-    </View>
+    <React.Fragment>
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+      <Toast />
+    </React.Fragment>
   );
 }
 
