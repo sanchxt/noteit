25d12500fa176bd9d7e1f3fdcc03d65b185d63b0
sanchxt <tpbeast007@gmail.com>
2025-01-09 17:26:14 +0530

    style header, add theme toggle

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,22 +4,42 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NotesList } from '../screens/NotesList';
 import { CreateNote } from '../screens/CreateNote';
 import { RootStackParamList } from '../types/navigation';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { ThemeToggle } from '../components/ThemeToggle';
+import { UpdateNote } from '../screens/UpdateNote';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const AppNavigator = () => {
+  const theme = useSelector((state: RootState) => state.theme.theme);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme === 'light' ? '#fff' : '#1C1C1E',
+          },
+          headerTintColor: theme === 'light' ? '#000' : '#fff',
+          headerTitleStyle: {
+            color: theme === 'light' ? '#000' : '#fff',
+          },
+        }}>
         <Stack.Screen
           name="NotesList"
           component={NotesList}
-          options={{ title: 'NoteIt' }}
+          options={{ title: 'NoteIt', headerRight: () => <ThemeToggle /> }}
         />
         <Stack.Screen
           name="CreateNote"
           component={CreateNote}
           options={{ title: 'New Note' }}
+        />
+        <Stack.Screen
+          name="UpdateNote"
+          component={UpdateNote}
+          options={{ title: 'Edit Note' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
